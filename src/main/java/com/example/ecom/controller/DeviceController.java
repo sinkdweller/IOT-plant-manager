bb69d6c556a6bcb1162c952817560eb7e5171006
Evelynn Lu <evie4697@gmail.com>
2026-01-30 14:13:54 -0500

    added remove device feature

--- src/main/java/com/example/ecom/controller/DeviceController.java
+++ src/main/java/com/example/ecom/controller/DeviceController.java
@@ -2,9 +2,10 @@ package com.example.ecom.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.ecom.entity.User;
 import com.example.ecom.dtos.ClaimDeviceDto;
@@ -20,32 +21,54 @@ public class DeviceController {
         this.deviceRepo=deviceRepo;
         this.userRepo=userRepo;
     }
-    @PostMapping("/claim")
-public ResponseEntity<String> claimDevice(@RequestBody ClaimDeviceDto dto) {
-    System.out.println("1. Starting claim for MAC: " + dto.getMacAddress());
+    @DeleteMapping("/remove")
+    public ResponseEntity<String> removeDevice(@RequestParam String macAddress){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println("User from token: (remove) " + username);
 
-    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User currentUser = userRepo.findByUsername(username)
+            .orElseThrow(() -> new RuntimeException("User not found in DB"));
 
-    User currentUser = userRepo.findByUsername(username)
-        .orElseThrow(() -> new RuntimeException("User not found in DB"));
+        
+        Device device = deviceRepo.findByMacAddress(macAddress).orElseThrow(()-> new RuntimeException("Device not found."));
+        if (device.getUser() == null || !device.getUser().equals(currentUser)) {
+            return ResponseEntity.status(409).body("You do not have permission to remove this device.");
+        }
+        deviceRepo.deleteByMacAddress(macAddress);
+        System.out.println("Device deleted");
+        return ResponseEntity.ok("Device "+macAddress + " deleted!");
 
-    Device device = deviceRepo.findByMacAddress(dto.getMacAddress())
-        .orElseGet(() -> {
-            Device d = new Device();
-            d.setMacAddress(dto.getMacAddress());
-            return d;
-        });
-
-    if (device.getUser() != null && !device.getUser().equals(currentUser)) {
-        return ResponseEntity.status(409).body("Already claimed by another user.");
     }
+    @PostMapping("/claim")
+    public ResponseEntity<String> claimDevice(@RequestBody ClaimDeviceDto dto) {
+        System.out.println("  Starting claim for MAC: " + dto.getMacAddress());
 
-    device.setName(dto.getName());
-    device.setUser(currentUser);
-    
-    deviceRepo.save(device);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println("User from token: " + username);
 
-    return ResponseEntity.ok("Device added!");
-}
+        User currentUser = userRepo.findByUsername(username)
+            .orElseThrow(() -> new RuntimeException("User not found in DB"));
+        System.out.println("Found User ID: " + currentUser.getId());
+
+        Device device = deviceRepo.findByMacAddress(dto.getMacAddress())
+            .orElseGet(() -> {
+                System.out.println("Creating brand new device record");
+                Device d = new Device();
+                d.setMacAddress(dto.getMacAddress());
+                return d;
+            });
+
+        if (device.getUser() != null && !device.getUser().equals(currentUser)) {
+            System.out.println("Already owned by someone else!");
+            return ResponseEntity.status(409).body("Already claimed by another user.");
+        }
+
+        device.setName(dto.getName());
+        device.setUser(currentUser);
+        
+        deviceRepo.save(device);
+
+        return ResponseEntity.ok("Device added!");
+    }
     
 }
--- src/main/java/com/example/ecom/repo/DeviceRepo.java
+++ src/main/java/com/example/ecom/repo/DeviceRepo.java
@@ -1,10 +1,18 @@
 package com.example.ecom.repo;
 
 import com.example.ecom.entity.Device;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
 import java.util.Optional;
 
 public interface DeviceRepo extends JpaRepository<Device, Long> {
     // Allows  Service to find the "Kitchen Fern" by its hardware ID
     Optional<Device> findByMacAddress(String macAddress);
+    @Transactional
+    @Modifying
+    void deleteByMacAddress(String macAddress);
 }
